204ea329666fcff13f306016d19ebb0ba31a00bf
Joe at Raposa <joe.isok@gmail.com>
2015-10-21 18:24:35 +0700

    Revised the grabcmd.php code and pre-pare to the new implementation

--- SMITPOS-WebServ/srpos_radius/controllers/grabcmd.php
+++ SMITPOS-WebServ/srpos_radius/controllers/grabcmd.php
@@ -1,31 +1,36 @@
-<html><body><h1>HACKED! Reply</h1>
-<p>This is the default web page for this server.</p>
-<p>The web server software is running but no content has been added, yet.</p>
-
-<?php echo "hello wOal --> ";
-      print_r($_POST);
-
-      xdebug_start_trace();
-	var_dump($_POST);
-      xdebug_stop_trace();
-?>
-
+<!-- 
+<html><body><h1>SRPOS Radius:: Grab Command Reply</h1>
+<p>This block for HTML to represent $_POST data
+in table.</p>
 <table>
-<?php
-
+<<!-- ?php
 	foreach ($_POST as $key => $value) {
 		echo "<tr>";
 		echo "<td>";
 		echo $key;
 		echo "</td>";
 		echo "<td>";
-                echo $value;
-                echo "</td>";
+        echo $value;
+        echo "</td>";
 		echo "</tr>";
 	}
-
 ?>
 </table>
-
 </body></html>
+-->
 
+<!-- ?php // This block is for debugging
+	xdebug_start_trace();
+	var_dump($_POST);
+	xdebug_stop_trace();
+? -->
+
+<?php // Main controller block to interface the service.
+	$wifiUser = $_POST['wifiUser'];
+	$expiration = $_POST['expiration'];
+
+	//echo $wifiUser . " :=: " . $expiration;
+	// Doing the task for set wifiUser expiration from here ...
+	// include_once ...
+	// if $expiration == "DEAD"
+?>
