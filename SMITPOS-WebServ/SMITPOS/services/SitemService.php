5b91a4f954f08152dc7fbf429b47f151e8a2b0fb
Chayes Deewanwong <chayes.d@thaistg.com>
2016-02-25 19:02:49 +0700

    Added sitemservice::get_item_stockView()

--- SMITPOS-WebServ/SMITPOS/services/SitemService.php
+++ SMITPOS-WebServ/SMITPOS/services/SitemService.php
@@ -917,23 +917,123 @@ class SitemService {
 
 		mysqli_stmt_bind_result($stmt, $row->itemIndex, $row->itemID, $row->itembarcodeID, $row->itemName, $row->itemDetail, $row->itemPrice, $row->itemLatestCost, $row->itemStock, $row->itemProPoint, $row->itemProStart, $row->itemProEnd, $row->CRE_DTE, $row->CRE_USR, $row->UPD_DTE, $row->UPD_USR, $row->DEL_DTE, $row->DEL_USR, $row->itemCatagoryIndex, $row->itemUnitIndex, $row->itemSizeIndex, $row->itemLocationIndex);
 
-	    while (mysqli_stmt_fetch($stmt)) {
-	      $row->itemProStart = new DateTime($row->itemProStart);
-	      $row->itemProEnd = new DateTime($row->itemProEnd);
-	      $row->CRE_DTE = new DateTime($row->CRE_DTE);
-	      $row->UPD_DTE = new DateTime($row->UPD_DTE);
-	      $row->DEL_DTE = new DateTime($row->DEL_DTE);
-	      $rows[] = $row;
-	      $row = new stdClass();
-	      mysqli_stmt_bind_result($stmt, $row->itemIndex, $row->itemID, $row->itembarcodeID, $row->itemName, $row->itemDetail, $row->itemPrice, $row->itemLatestCost, $row->itemStock, $row->itemProPoint, $row->itemProStart, $row->itemProEnd, $row->CRE_DTE, $row->CRE_USR, $row->UPD_DTE, $row->UPD_USR, $row->DEL_DTE, $row->DEL_USR, $row->itemCatagoryIndex, $row->itemUnitIndex, $row->itemSizeIndex, $row->itemLocationIndex);
-	    }
+		while (mysqli_stmt_fetch($stmt)) {
+			$row->itemProStart = new DateTime($row->itemProStart);
+			$row->itemProEnd = new DateTime($row->itemProEnd);
+			$row->CRE_DTE = new DateTime($row->CRE_DTE);
+			$row->UPD_DTE = new DateTime($row->UPD_DTE);
+			$row->DEL_DTE = new DateTime($row->DEL_DTE);
+			$rows[] = $row;
+			$row = new stdClass();
+			mysqli_stmt_bind_result($stmt, $row->itemIndex, $row->itemID, $row->itembarcodeID, $row->itemName, $row->itemDetail, $row->itemPrice, $row->itemLatestCost, $row->itemStock, $row->itemProPoint, $row->itemProStart, $row->itemProEnd, $row->CRE_DTE, $row->CRE_USR, $row->UPD_DTE, $row->UPD_USR, $row->DEL_DTE, $row->DEL_USR, $row->itemCatagoryIndex, $row->itemUnitIndex, $row->itemSizeIndex, $row->itemLocationIndex);
+		}
 
 		mysqli_stmt_free_result($stmt);
-	    mysqli_close($this->connection);
+		mysqli_close($this->connection);
 
-	    return $rows;
+		return $rows;
 	}
 
+	public function get_item_stockView($year,$month) {
+		// stmt = statement
+		$stmt = mysqli_prepare($this->connection,"
+		SELECT
+		`tb_i`.`itemIndex`,
+		`tb_i`.`itemID`,
+		`tb_i`.`itemName`,
+		IFNULL(`tb_startQTY`.`startQTY`,0) as 'startQTY',
+		IFNULL(`tb_in_sum`.`sum_inQTY`,0) as 'sum_inQTY',
+		IFNULL(`tb_sl_sum`.`sum_saleQTY`,0) as 'sum_saleQTY',
+		IFNULL(`tb_move`.`stockQTY`,0) as 'remainQTY'
+		FROM
+		`_item` AS tb_i
+		LEFT JOIN (SELECT `in_in`.`itemIndex`, SUM(`in_in`.`inQTY`) as 'sum_inQTY'
+			FROM `inventoryin` as in_in
+				WHERE YEAR(`in_in`.`CRE_DTE` ) = $year AND
+				MONTH(`in_in`.`CRE_DTE` ) = $month
+			GROUP BY `in_in`.`itemIndex`
+			) AS tb_in_sum ON `tb_in_sum`.`itemIndex` = `tb_i`.`itemIndex`
+		LEFT JOIN (SELECT `sl`.`itemIndex`, SUM(`sl`.`saleQTY`) as 'sum_saleQTY'
+			FROM `salelist` as sl
+				WHERE YEAR(`sl`.`CRE_DTE` ) = $year AND
+				MONTH(`sl`.`CRE_DTE` )  = $month
+				GROUP BY `sl`.`itemIndex`
+				) AS tb_sl_sum ON `tb_sl_sum`.`itemIndex` = `tb_i`.`itemIndex`
+		LEFT JOIN ( SELECT `in_mov`.`itemIndex`, SUM(`in_mov`.`moveQTY`) as 'sum_moveQTY'
+			FROM `inventorymove` as in_mov
+				WHERE YEAR(`in_mov`.`CRE_DTE` ) = $year AND
+				MONTH(`in_mov`.`CRE_DTE` ) = $month
+				GROUP BY `in_mov`.`itemIndex`
+				) AS tb_mov_sum ON `tb_mov_sum`.`itemIndex` = `tb_i`.`itemIndex`
+		LEFT JOIN (SELECT `itemIndex` as `iIndex`,`stockQTY`
+			FROM `itemmovementview` as `i_movement`
+			INNER JOIN
+					(
+					SELECT MAX(CRE_DTE) as MAXDate , `imo`.`itemIndex` as sIndex
+					FROM `itemmovementview` as `imo`
+					WHERE YEAR(CRE_DTE) = $year
+					AND MONTH(CRE_DTE) = $month
+					GROUP BY `imo`.`itemIndex`
+					) AS  `tb_movement_y`
+					ON  `i_movement`. `itemIndex` =  `tb_movement_y`.`sIndex`
+					AND `i_movement`.`CRE_DTE` = `tb_movement_y`.`MAXDate`
+					ORDER BY `i_movement`.`itemIndex`) AS `tb_move` ON `tb_move`.`iIndex` = `tb_i`.`itemIndex`
+		LEFT JOIN(
+		SELECT
+		`itemIndex` , `actionType`,
+		IF(actionType LIKE 'IN%','สถานะเข้า','สถานะออก') as 'status',
+		IF(actionType LIKE 'IN%',`stockQTY` - `QTY`,`stockQTY` + `QTY`) as 'startQTY'
+		FROM `itemmovementview` as `i_movement`
+			INNER JOIN
+			(
+			SELECT MIN(CRE_DTE) as MINDate , `imo`.`itemIndex` as sIndex
+			FROM `itemmovementview` as `imo`
+		WHERE YEAR(CRE_DTE) = $year
+			AND MONTH(CRE_DTE) = $month
+			GROUP BY `imo`.`itemIndex`
+			) AS  `tb_movement_y`
+			ON  `i_movement`. `itemIndex` =  `tb_movement_y`.`sIndex`
+			AND `i_movement`.`CRE_DTE` = `tb_movement_y`.`MINDate`
+		ORDER BY `i_movement`.`itemIndex`) as `tb_startQTY` ON `tb_startQTY`.`itemIndex` = `tb_i`.`itemIndex`
+		");
+		$this->throwExceptionOnError();
+
+		mysqli_stmt_execute($stmt);
+		$this->throwExceptionOnError();
+
+		// Empty array
+		$rows = array();
+
+		// Bind Data
+		mysqli_stmt_bind_result($stmt,
+		$row->itemIndex,
+		$row->itemID,
+		$row->itemName,
+		$row->startQTY,
+		$row->sum_inQTY,
+		$row->sum_saleQTY,
+		$row->remainQTY
+		);
+
+		// Retrive Data
+		while(mysqli_stmt_fetch($stmt)){
+			$rows[] = $row;
+			$row = new stdClass(); /* Empty object of Dynamic Class*/
+			mysqli_stmt_bind_result($stmt,
+			$row->itemIndex,
+			$row->itemID,
+			$row->itemName,
+			$row->startQTY,
+			$row->sum_inQTY,
+			$row->sum_saleQTY,
+			$row->remainQTY
+		);
+		}
+
+		mysqli_stmt_free_result($stmt);
+		mysqli_close($this->connection);
+		return $rows;
+	}
 }
 
 ?>
